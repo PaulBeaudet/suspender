c585d54dbe0b71d5cdd992d89e81f81e4677ca01
PaulBeaudet <inof8or@gmail.com>
2018-02-15 16:14:08 -0500

    Reception logic

--- suspenders.ino
+++ suspenders.ino
@@ -7,16 +7,45 @@
 JS_Timer timer = JS_Timer(); // create an instance of our timer object from timer library
 
 void setup() {
-  Keyboard.begin();          // allows to act as USB HID device
-  timer.setTimeout(erm, 1000); // set command in x amount of time
+  Keyboard.begin();            // allows to act as USB HID device
+  Serial.begin(115200);        // comunicate with server
+  // timer.setTimeout(erm, 1000); // set command in x amount of time
 }
- 
- 
+
 void loop() {
   timer.todoChecker();
+  int newTime = recieveNextTime();
+  if(newTime){
+      timer.setTimeout(erm, newTime);
+  }
 }
 
+//---- Functions ------
 void erm(){
-  Keyboard.print("ERM");  
+  Serial.println("mama");
+  Keyboard.print("mama");
 }
 
+//======================== Serial Data Transfer (INTERFACE)
+#define START_MARKER '<'
+#define END_MARKER '>'
+
+int recieveNextTime(){
+  static String placeholder= "";
+  static boolean inProgress = false;  // note if read started
+
+  if(Serial.available()) {            // is there anything to be read
+    char readChar = Serial.read();    // yes? read it
+    if(inProgress){                   // did we read start maker?
+      if(readChar == END_MARKER){     // did we read end marker
+        inProgress = false;               // note finished
+        int result = placeholder.toInt(); // return int
+        placeholder = "";
+        return result;
+      } else {                        // given still in progress
+        placeholder += readChar;      // concat this char
+      }
+    } else if(readChar == START_MARKER){inProgress = true;} // indicate when to read when start marker is seen
+  }
+  return 0; // in the case the message has yet to be recieved
+}
